9e238f781c864ff27ce21f1959cc65a951c4f980
Nathan Podesta <nathan.podesta@icloud.com>
2022-04-20 14:34:52 +0200

    exo 2

---
+++ day-1/exercise-2.js
@@ -0,0 +1,7 @@
+import { my_display_alpha } from "./exercise-1.js";
+
+export const my_display_alpha_reverse = () => {
+    return my_display_alpha();
+};
+
+console.log(my_display_alpha_reverse());
