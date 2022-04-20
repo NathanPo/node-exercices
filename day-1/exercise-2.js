7584fcee2ae2c9847b12d7b215ee398780d43f1b
Nathan Podesta <nathan.podesta@icloud.com>
2022-04-20 14:50:26 +0200

    fix exo 2

--- day-1/exercise-2.js
+++ day-1/exercise-2.js
@@ -1,7 +1,12 @@
 import { my_display_alpha } from "./exercise-1.js";
 
 export const my_display_alpha_reverse = () => {
-    return my_display_alpha();
+    let b = my_display_alpha();
+    var plop = new String();
+    for (var i = 25; i >= 0; i--) {
+        plop += b[i];
+    }
+    return plop;
 };
 
 console.log(my_display_alpha_reverse());
