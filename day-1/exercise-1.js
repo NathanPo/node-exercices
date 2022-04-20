9b6ba3fcb3c6e44b0544ab7441202a7a56b5c472
Nathan Podesta <nathan.podesta@icloud.com>
2022-04-20 14:33:29 +0200

    exo 1

---
+++ day-1/exercise-1.js
@@ -0,0 +1,10 @@
+export const my_display_alpha = () => {
+    var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var orderedAlpha = new String();
+    for (let i = 0; i <= 25; i++) {
+        orderedAlpha += alpha[i];
+    }
+    return orderedAlpha;
+};
+
+console.log(my_display_alpha());
